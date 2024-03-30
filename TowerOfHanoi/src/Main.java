8a1efb8e55886315b63120255c28ee668e7fb3a9
SomyaSikarwar <118437254+SomyaSikarwar@users.noreply.github.com>
2024-03-30 14:51:08 +0530

    Added Tower Of Hanoi

--- TowerOfHanoi/src/Main.java
+++ TowerOfHanoi/src/Main.java
@@ -20,3 +20,45 @@ public class Main {
         hanoi.towerOfHanoi(n, source, destination, auxiliary);
     }
 }
+/****************************output******************************
+    Initial State:
+A: 1 2 3 
+C: 
+B: 
+------
+Steps to solve Tower of Hanoi problem with 3 disks:
+Move disk 1 from A to C
+A: 2 3 
+C: 1 
+B: 
+------
+Move disk 2 from A to B
+A: 3 
+B: 2 
+C: 1 
+------
+Move disk 1 from C to B
+C: 
+B: 1 2 
+A: 3 
+------
+Move disk 3 from A to C
+A: 
+C: 3 
+B: 1 2 
+------
+Move disk 1 from B to A
+B: 2 
+A: 1 
+C: 3 
+------
+Move disk 2 from B to C
+B: 
+C: 2 3 
+A: 1 
+------
+Move disk 1 from A to C
+A: 
+C: 1 2 3 
+B: 
+------*/
